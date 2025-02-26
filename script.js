ae4056677dd8e52aca73a17d306e5a22c445e839
Student <roscovancostel@gmail.com>
2025-02-26 08:22:14 +0200

    Add links object containing my links

---
+++ script.js
@@ -0,0 +1,46 @@
+
+const container = document.getElementById("links-container")
+const links = [
+    {
+        name: "Etch A Sketch",
+        link: "https://woodenbrick1.github.io/Etch_A_Sketch/",
+        // image: 
+    },
+    {
+        name: "Tic-Tac-Toe",
+        link: "https://woodenbrick1.github.io/Tic-Tac-Toe/",
+        // image:
+    },
+    {
+        name: "To Do List",
+        link: "http://woodenbrick1.github.io/To-do-List/",
+        // image:
+    },
+    {
+        name: "Ice Cream",
+        link: "https://woodenbrick1.github.io/Restaurant-Page/",
+        // image:
+    },
+    {
+        name: "Library",
+        link: "https://woodenbrick1.github.io/Library/",
+        // image:
+    },
+    {
+        name: "Calculator",
+        link: "https://woodenbrick1.github.io/Calculator/",
+        // image:
+    },
+    {
+        name: "Weather App",
+        link: "https://woodenbrick1.github.io/Weather-App/",
+        // image:
+    }
+    
+    
+]
+
+
+window.addEventListener("DOMContentLoaded", () =>{
+
+});
